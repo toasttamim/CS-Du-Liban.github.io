77bc8627c54688b7ce32170e07ef772154a51cd7
toasttamim <nour.tamim@st.ul.edu.lb>
2025-02-02 11:23:04 +0200

    Create game-selection.js

---
+++ game-selection.js
@@ -0,0 +1,9 @@
+//takes the user to our card game 
+ document.getElementById('game1-btn').addEventListener('click', function() {
+    window.location.href = 'game1.html'; 
+  });
+
+  // takes the user to our spin the wheel game
+  document.getElementById('game2-btn').addEventListener('click', function() {
+    window.location.href = 'spin-wheel.html'; 
+  });
